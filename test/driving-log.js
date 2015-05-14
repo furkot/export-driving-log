2608ab6c348e5d3c135e38512a3b10f79d587bec
Damian Krzeminski <pirxpilot@code42day.com>
2015-05-13 20:42:38 -0400

    implementation and tests

--- test/driving-log.js
+++ test/driving-log.js
@@ -1,9 +1,55 @@
-var should = require('should');
-var furkotDrivingLog = require('../');
+var fs = require('fs');
+var path = require('path');
+var csv = require('..');
+
+/**
+ * Compare files line-by-line
+ */
+function compareCsv(actual, expected) {
+  var a = actual.split('\n');
+  var e = expected.split('\n');
+  a.should.have.length(e.length);
+  a.forEach(function(line, index) {
+    line.should.eql(e[index]);
+  });
+}
+
+function readFileSync(name) {
+  return fs.readFileSync(path.join(__dirname, name), 'utf8');
+}
 
 describe('furkot-driving-log node module', function () {
-  it('must have at least one test', function () {
-    furkotDrivingLog();
-    should.fail('Need to write tests.');
+
+  it('simple trip', function() {
+    var t = require('./fixtures/simple-trip.json'),
+      generated = csv(t),
+      expected = readFileSync('fixtures/simple.csv');
+
+    compareCsv(generated, expected);
   });
+
+  it('simple trip with adjusted speed', function() {
+    var t = require('./fixtures/speed-trip.json'),
+      generated = csv(t),
+      expected = readFileSync('fixtures/speed.csv');
+
+    compareCsv(generated, expected);
+  });
+
+  it('multi trip', function() {
+    var t = require('./fixtures/multi-trip.json'),
+      generated = csv(t),
+      expected = readFileSync('fixtures/multi.csv');
+
+    compareCsv(generated, expected);
+  });
+
+  it('begin end time', function() {
+    var t = require('./fixtures/time-trip.json'),
+    generated = csv(t),
+    expected = readFileSync('fixtures/time.csv');
+
+    compareCsv(generated, expected);
+  });
+
 });
